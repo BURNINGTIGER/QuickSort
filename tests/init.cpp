3cb228d5b054f0da513556ffdebde26eb4202736
BURNINGTIGER <Aleks203467@gmail.com>
2017-06-08 10:55:15 +0300

    Create init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -5,8 +5,8 @@ SCENARIO("sort","[sort]")
 {
  	int array[6] = { 2 , 1, 6, 3, 7, 4 };
 	int sortedArray[6] =  { 1, 2, 3, 4, 6, 7 };
-	int* first = &matrix[0];
-	int* last = &matrix[6];
+	int* first = &array[0];
+	int* last = &array[6];
 	QuickSort(first, last);
 	bool result = true;
 	for (int i = 0; i < 6; i++)
